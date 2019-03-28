59c8ec35faf41d6fa170d8a8cec2b77e77b62f03
DESKTOP-NDGE3EJ\CarlosLap <condesa.sys@gmail.com>
2019-03-28 12:29:10 -0600

    Add use static files.

--- GoogleMapsAPIGetStarted/Startup.cs
+++ GoogleMapsAPIGetStarted/Startup.cs
@@ -25,10 +25,8 @@ namespace GoogleMapsAPIGetStarted
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseStaticFiles();
+
         }
     }
 }
